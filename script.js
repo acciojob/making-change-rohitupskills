3c4aae092e2a4f2f3c193efcc9fb95539acdab0a
rohitupskills <130683027+rohitupskills@users.noreply.github.com>
2023-11-15 23:18:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const makeChange = (c) => {
   // your name here
+	let units = {
+		q : 25,
+		d : 10,
+		n : 5,
+		p : 1
+	}
+	let output = {};
+
+	for(let i in units){
+		let currentUnit = units[i];
+		output[i] = num / currentUnit;
+		num = num - currentUnit * output[i]
+		return output;
+	}
 };
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+alert(JSON.stringify(makeChange(c))); 
